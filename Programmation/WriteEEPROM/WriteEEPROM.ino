0ee48a46239bec682ed68df04718f7a0afb776e0
Savoie13 <89605283+Savoie13@users.noreply.github.com>
2023-04-11 11:15:54 -0400

    Update WriteEEPROM.ino
    Commented WriteEEPROM.ino

--- Programmation/WriteEEPROM/WriteEEPROM.ino
+++ Programmation/WriteEEPROM/WriteEEPROM.ino
@@ -1,11 +1,12 @@
 /*
   WriteEEPROM
 
-  Code qui doit être téléchargé avant de télécharger Full_controllerV5.ino. Il permet de
-  mettre des valeurs au bonnes adresses de l'EEPROM du Arduino Pro Micro pour que lors du
-  téléchargement du code principal "Full_controllerV5" les boutons de la manette aies des
-  valeurs de base. Dans ce code on met les boutons 0 à 14 à a, w, s, "clic gauche", d, 1,
-  2, 3, 4, 5, 6, 7, 8, 9 respectivement.
+  Code qui doit être téléchargé avant de télécharger Full_controllerV5.ino disponible au
+  https://github.com/Savoie13/Manette_Adaptee/tree/main/Programmation/Full_controllerV5. 
+  Il permet de mettre des valeurs au bonnes adresses de l'EEPROM du Arduino Pro Micro pour
+  que lors du téléchargement du code principal "Full_controllerV5" les boutons de la 
+  manette aies des valeurs de base. Dans ce code on met les boutons 0 à 14 à a, w, s, 
+  "clic gauche", d, 1, 2, 3, 4, 5, 6, 7, 8, 9 respectivement.
     
   Hardware:
   - Arduino Micro Pro.
